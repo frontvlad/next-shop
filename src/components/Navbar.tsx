c44a07e32ff9dc5d1b70549da5b6685f7be07493
Vlad Sidyukov <sidyukov.vladislav@gmail.com>
2024-11-11 19:43:35 +0700

    commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+import Menu from "./Menu";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="h-full flex items-center justify-between">
+        {/* MOBILE */}
+        <Link href="/">
+          <div className="text-2xl tracking-wide">Vlad</div>
+        </Link>
+        <Menu></Menu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
